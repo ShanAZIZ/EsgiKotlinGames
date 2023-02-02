e1400b75a93f06698ad1b1bedda455b6757640cd
saziz <shan.azizh@gmail.com>
2023-02-02 10:17:11 +0100

    game detail app visual

--- app/src/main/java/fr/esgi/games/home/detail/GameDetailsFragment.kt
+++ app/src/main/java/fr/esgi/games/home/detail/GameDetailsFragment.kt
@@ -5,14 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import fr.esgi.games.R
+import fr.esgi.games.service.FirebaseLikesService
+import fr.esgi.games.service.GameApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class GameDetailsFragment : Fragment() {
 
     private lateinit var navController: NavController
+    private lateinit var firebaseLikesService: FirebaseLikesService
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,10 +39,32 @@ class GameDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         navController = Navigation.findNavController(view)
+        firebaseLikesService = FirebaseLikesService()
+        auth = Firebase.auth
         val args: GameDetailsFragmentArgs by navArgs()
+
+        val like = view.findViewById<View>(R.id.toolbar_like)
+
+        isLiked(args.appId, like)
 
         view.findViewById<View>(R.id.toolbar_cross).setOnClickListener {
             navController.popBackStack()
+        }
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val game = GameApiService.fetchGameDetail(args.appId)
+            if(game != null) {
+                withContext(Dispatchers.Main) {
+                    val bgImage = view.findViewById<ImageView>(R.id.game_details_background)
+                    val cardBgImage = view.findViewById<ImageView>(R.id.card_bg_image)
+                    val cardImage = view.findViewById<ImageView>(R.id.card_image)
+                    Glide.with(bgImage.context).load(game.getBgrImage()).centerCrop().into(bgImage) // TODO loader
+                    Glide.with(cardBgImage.context).load(game.bgImage).centerCrop().into(cardBgImage) // TODO loader
+                    Glide.with(cardImage.context).load(game.getSmallImage()).centerCrop().into(cardImage) // TODO loader
+                    view.findViewById<TextView>(R.id.game_title).text= game.title
+                    view.findViewById<TextView>(R.id.game_editor).text= game.editor
+                }
+            }
         }
 
         // TODO: init is in whishlist and is in liked
@@ -35,6 +72,15 @@ class GameDetailsFragment : Fragment() {
         // TODO: add to whishlist / remove
         // TODO: retrieve game info
         // TODO: description / avis
+    }
 
+    private fun isLiked(id: Int, like: View) {
+        println(id)
+        val uid = auth.currentUser?.uid ?: return
+        firebaseLikesService.isInLikes(uid, id) {
+            if(it) {
+                like.background = context?.let { it1 -> ContextCompat.getDrawable(it1, R.drawable.like_full) }
+            }
+        }
     }
 }
--- app/src/main/java/fr/esgi/games/model/GameDetail.kt
+++ app/src/main/java/fr/esgi/games/model/GameDetail.kt
@@ -16,4 +16,9 @@ data class GameDetail(
         val url =  String.format("http://cdn.origin.steamstatic.com/steam/apps/%s/library_600x900.jpg?t=1673331963", appId.toString())
         return url.toUri().buildUpon().scheme("http").build()
     }
+
+    fun getBgrImage(): Uri? {
+        val url = String.format("https://cdn.akamai.steamstatic.com/steam/apps/%s/page_bg_generated.jpg?t=1668125812", appId.toString())
+        return url.toUri().buildUpon().scheme("https").build()
+    }
 }
