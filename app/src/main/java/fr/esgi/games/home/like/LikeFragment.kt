e827f9756635f63121030ab5cc20698bdc283985
saziz <shan.azizh@gmail.com>
2023-02-02 11:16:40 +0100

    likes from firebase

--- app/src/main/java/fr/esgi/games/home/like/LikeFragment.kt
+++ app/src/main/java/fr/esgi/games/home/like/LikeFragment.kt
@@ -15,12 +15,13 @@ import com.google.firebase.ktx.Firebase
 import fr.esgi.games.R
 import fr.esgi.games.home.CustomOnClickListener
 import fr.esgi.games.home.GameRecyclerAdapter
+import fr.esgi.games.service.FirebaseLikesService
 
 class LikeFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var navController: NavController
-    private var gameIds : ArrayList<Int> = ArrayList(730)
+    private lateinit var firebaseLikesService: FirebaseLikesService
 
 
     override fun onCreateView(
@@ -35,6 +36,7 @@ class LikeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         auth = Firebase.auth
         navController = Navigation.findNavController(view)
+        firebaseLikesService = FirebaseLikesService()
 
         val exitButton = view.findViewById<View>(R.id.toolbar_cross)
         val recyclerView =view.findViewById<RecyclerView>(R.id.like_list)
@@ -42,24 +44,32 @@ class LikeFragment : Fragment() {
         exitButton.setOnClickListener {
             navController.navigate(R.id.action_likeFragment_to_homeFragment)
         }
-        if(gameIds.isEmpty()){
-            view.findViewById<View>(R.id.no_likes).visibility = View.VISIBLE
-        }
-        else{
-            recyclerView.visibility = View.VISIBLE
-            view.findViewById<View>(R.id.no_likes).visibility = View.GONE
-            view.findViewById<View>(R.id.click).visibility = View.GONE
-            view.findViewById<View>(R.id.likes_bg_svg).visibility = View.GONE
 
-        }
-        val adapter = GameRecyclerAdapter(gameIds, object: CustomOnClickListener<Int> {
-            override fun onClick(item: Int) {
-                navController.navigate(R.id.action_likeFragment_to_gameDetailsFragment)
+
+        val uid = auth.uid
+
+        if(uid != null){
+            firebaseLikesService.getLikes(uid).addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    if(task.result.isEmpty()){
+                        view.findViewById<View>(R.id.no_likes).visibility = View.VISIBLE
+                    }
+                    else{
+                        recyclerView.visibility = View.VISIBLE
+                        view.findViewById<View>(R.id.no_likes).visibility = View.GONE
+                        view.findViewById<View>(R.id.click).visibility = View.GONE
+                        view.findViewById<View>(R.id.likes_bg_svg).visibility = View.GONE
+                        val adapter = GameRecyclerAdapter(task.result, object: CustomOnClickListener<Int> {
+                            override fun onClick(item: Int) {
+                                val action = LikeFragmentDirections.actionLikeFragmentToGameDetailsFragment(item)
+                                navController.navigate(action)
+                            }
+                        })
+                        recyclerView.adapter = adapter
+                        recyclerView.layoutManager = LinearLayoutManager(context)
+                    }
+                }
             }
-        })
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(context)
+        }
     }
-
-
 }
---
+++ app/src/main/java/fr/esgi/games/service/FirebaseLikesService.kt
@@ -0,0 +1,38 @@
+package fr.esgi.games.service
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class FirebaseLikesService {
+
+    private val documentName: String = "users"
+    private val db: FirebaseFirestore = Firebase.firestore
+
+    fun getLikes(userId: String): Task<ArrayList<Int>> {
+        return db.collection(documentName).document(userId)
+            .get()
+            .continueWith { task ->
+                if (task.isSuccessful) {
+                    val documentSnapshot = task.result
+                    if (documentSnapshot != null && documentSnapshot.exists()) {
+                        convertArrayList(documentSnapshot.get("liked") as ArrayList<*>)
+                    } else {
+                        throw Exception("Document does not exist")
+                    }
+                } else {
+                    throw task.exception!!
+                }
+            }
+    }
+
+    private fun convertArrayList(arrayList: ArrayList<*>): ArrayList<Int> {
+        val intArrayList = ArrayList<Int>()
+        for (item in arrayList) {
+            val i = item as Long
+            intArrayList.add(i.toInt())
+        }
+        return intArrayList
+    }
+}
